fba87847704c96e9298190127ed17cdece4af567
Ulises <ulises.argueta95@gmail.com>
2023-03-14 16:15:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  } else if ( someValue >= 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  } else if ( someValue >= 2000 && someValue <= 2500 ){
+    return 'I will gladly take your thirty bucks.'
+  } else {
+    return 'No can do.'
+  }
+} 
+
+function ternaryCheckCity(someCity){
+  return someCity === 'NYC'?'Ok, sounds good.':'No go.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    case 'thanks for everything':
+      return'Bye.'
+  }
 }
